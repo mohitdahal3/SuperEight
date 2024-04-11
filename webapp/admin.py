08567bcdd4eeb2bc4f5cd80ffddc6fc9b1ea0c50
Mohit Dahal <mohitdahal3@gmail.com>
2024-04-11 15:09:51 +0545

    modified admin panel

--- webapp/admin.py
+++ webapp/admin.py
@@ -2,4 +2,23 @@ from django.contrib import admin
 
 # Register your models here.
 from webapp.models import Row
-admin.site.register(Row)
+
+class RowAdmin(admin.ModelAdmin):
+    list_display = ('get_custom_index', 'column1', 'column2', 'column3', 'column4', 'column5' , 'column6' , 'column7')
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.order_by('id')
+    
+    def get_custom_index(self, obj):
+        # Get all ids of rows in ascending order
+        all_ids = list(Row.objects.values_list('id', flat=True))
+        # Find index of current row's id within all_ids list
+        try:
+            index = all_ids.index(obj.id) + 1  # Adding 1 to start indexing from 1
+        except ValueError:
+            index = None
+        return index
+
+    get_custom_index.short_description = 'Index'
+
+admin.site.register(Row , RowAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
 class Row(models.Model):
     column1 = models.CharField(max_length=100 , blank=True , null=True , default="")
     column2 = models.CharField(max_length=100 , blank=True , null=True , default="")
     column3 = models.CharField(max_length=100 , blank=True , null=True , default="")
     column4 = models.CharField(max_length=100 , blank=True , null=True , default="")
     column5 = models.CharField(max_length=100 , blank=True , null=True , default="")
+    column6 = models.CharField(max_length=100 , blank=True , null=True , default="")
+    column7 = models.CharField(max_length=100 , blank=True , null=True , default="")
 
     def __str__(self):
-        return "Row"
+        return f"Row"
